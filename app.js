31f52bbb103c1eb8406d78ba786fa30e7ab5a6e9
tahbje <noljko13@gmail.com>
2024-04-11 14:24:35 -0300

    added the contact section

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const hamMenu = document.querySelector(".ham-menu");
 
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
-/* document.addEventListener("click", () => {
-  hamMenu.classList.remove("ham-menu-active");
-  offScreenMenu.classList.remove("ham-menu-active");
-});
- */
-hamMenu.addEventListener("click", () => {
+let toggleMenu = () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
-});
+};
+
+let removeMenu = () => {
+  hamMenu.classList.remove("active");
+  offScreenMenu.classList.remove("active");
+};
+
+hamMenu.addEventListener("click", toggleMenu);
+offScreenMenu.addEventListener("click", removeMenu);
